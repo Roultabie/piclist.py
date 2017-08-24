1dedc41f7727501dbe589b36f9757f56f84dc653
Daniel Douat <daniel.douat@gorgones.net>
2017-08-24 23:36:05 +0200

    optimized code

--- piclist.py
+++ piclist.py
@@ -13,9 +13,13 @@ thumbRatio = [4, 3]
 publicBase = ""
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dir", type=str, help="spécifie le répertoire à traiter")
-parser.add_argument("--base", type=str, help="spécifie la raçine publique de la galerie")
-parser.add_argument("-r", "--regenerate", help="force la génération complète du répertoire", action="store_true")
+parser.add_argument("--dir", type=str,\
+                    help="spécifie le répertoire à traiter")
+parser.add_argument("--base", type=str,\
+                    help="spécifie la raçine publique de la galerie")
+parser.add_argument("-r", "--regenerate",\
+                    help="force la génération complète du répertoire",\
+                    action="store_true")
 args = parser.parse_args()
 
 SCRIPT_PATH = os.path.abspath(__file__)
@@ -28,7 +32,8 @@ if os.path.exists(os.path.join(SCRIPT_PATH,"/config.cfg")):
 GALLERY_PATH = args.dir if args.dir else os.path.join(SCRIPT_PATH,galleryDir)
 PUBLIC_BASE = args.base.rstrip("/") if args.base else publicBase.rstrip("/")
 GALLERY_DIR = os.path.basename(PUBLIC_BASE) if PUBLIC_BASE else galleryDir
-TEMPLATE_PATH = os.path.join(GALLERY_PATH,templateDir) if os.path.isdir(os.path.join(GALLERY_PATH,templateDir)) else templateDir
+TEMPLATE_PATH = os.path.join(GALLERY_PATH,templateDir)\
+    if os.path.isdir(os.path.join(GALLERY_PATH,templateDir)) else templateDir
 
 def generate(dirPath="",currentDir="",ariane="",privateBaseList="",dirs=""):
     """Set content of html templates in respective vars"""
@@ -45,6 +50,8 @@ def generate(dirPath="",currentDir="",ariane="",privateBaseList="",dirs=""):
 
     dirPath = os.path.normpath(dirPath) if dirPath else GALLERY_PATH
     currentDir = currentDir if currentDir else GALLERY_DIR
+
+    print(directory)
 
 def get_content(path):
     with open(path, "r") as content_file:
