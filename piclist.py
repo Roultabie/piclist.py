ce592b510b3e0565e40c5ef2035661f5b1677c04
Daniel Douat <daniel.douat@gorgones.net>
2017-08-24 21:49:47 +0200

    added first work (vars init) of piclist.py

---
+++ piclist.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import argparse
+import re
+
+imagePattern = "/([^.]+)\.(jpg|png|gif)/Ui"
+galleryDir = "gallery"
+templateDir = "template"
+thumbsDir = "_thumbs"
+thumbsPath = galleryDir + "/" + thumbsDir
+thumbWidth = [200]
+thumbRatio = [4, 3]
+publicBase = ""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir", type=str, help="spécifie le répertoire à traiter")
+parser.add_argument("--base", type=str, help="spécifie la raçine publique de la galerie")
+parser.add_argument("-r", "--regenerate", help="force la génération complète du répertoire", action="store_true")
+args = parser.parse_args()
+
+SCRIPT_PATH = os.path.abspath(__file__)
+
+if os.path.exists(SCRIPT_PATH + "/config.cfg"):
+    import configparser
+    config = ConfigParser.ConfigParser
+    config.read(SCRIPT_PATH + "/config.cfg")
+
+GALLERY_PATH = args.dir if args.dir else SCRIPT_PATH + "/" + galleryDir
+PUBLIC_BASE = args.base.rstrip("/") if args.base else publicBase.rstrip("/")
+GALLERY_DIR = os.path.basename(PUBLIC_BASE) if PUBLIC_BASE else galleryDir
+TEMPLATE_PATH = GALLERY_PATH + "/" + templateDir if os.path.isdir(GALLERY_PATH + "/" + templateDir) else templateDir
+
