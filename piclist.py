f134c18149c4987278641715dcf6b6cc3e589ced
Daniel Douat <daniel.douat@gorgones.net>
2017-08-24 23:31:13 +0200

    added templates vars in generate function

--- piclist.py
+++ piclist.py
@@ -30,9 +30,25 @@ PUBLIC_BASE = args.base.rstrip("/") if args.base else publicBase.rstrip("/")
 GALLERY_DIR = os.path.basename(PUBLIC_BASE) if PUBLIC_BASE else galleryDir
 TEMPLATE_PATH = os.path.join(GALLERY_PATH,templateDir) if os.path.isdir(os.path.join(GALLERY_PATH,templateDir)) else templateDir
 
-print(os.path.join(GALLERY_PATH,templateDir))
+def generate(dirPath="",currentDir="",ariane="",privateBaseList="",dirs=""):
+    """Set content of html templates in respective vars"""
+    page = get_content(get_template_path("index.html"))\
+            if os.path.exists(get_template_path("index.html")) else ""
+    imageTag = get_content(get_template_path("imagetag.html"))\
+            if os.path.exists(get_template_path("imagetag.html")) else ""
+    directory = get_content(get_template_path("directory.html"))\
+            if os.path.exists(get_template_path("directory.html")) else ""
+    arianeTag = get_content(get_template_path("ariane.html"))\
+            if os.path.exists(get_template_path("ariane.html")) else ""
+    exifTag = get_content(get_template_path("exif.html"))\
+            if os.path.exists(get_template_path("exif.html")) else ""
+
+    dirPath = os.path.normpath(dirPath) if dirPath else GALLERY_PATH
+    currentDir = currentDir if currentDir else GALLERY_DIR
 
 def get_content(path):
     with open(path, "r") as content_file:
-        content = content_file.read()
+        return content_file.read()
 
+def get_template_path(file_name):
+    return os.path.join(TEMPLATE_PATH,file_name)
